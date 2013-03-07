d15e534aef30042d8dfb0e95719cede85db39784
Martin Rychlovsky <masak@gravastar.cz>
2013-03-07 14:10:32 +0100

    Upgraded PrettyPhoto to version 3.1.5 => works with jQuery 1.9.1 and Spree 1.3

--- app/assets/javascripts/store/spree_prettyphoto.js
+++ app/assets/javascripts/store/spree_prettyphoto.js
@@ -1,4 +1,3 @@
-//= require store/spree_core
 //= require store/jquery.prettyPhoto
 $(document).ready(function(){
   $("a[rel^='lightbox']").prettyPhoto();
--- lib/spree_prettyphoto.rb
+++ lib/spree_prettyphoto.rb
@@ -9,8 +9,8 @@ Dir.glob(File.join(File.dirname(__FILE__), "../app/**/*_decorator*.rb")) do |c|
 end
 end
 
-def load_tasks
-end
+#def load_tasks
+#end
 
 config.to_prepare &method(:activate).to_proc
 end
