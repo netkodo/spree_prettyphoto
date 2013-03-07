72580d5698caeebfc2201b4524b88eeb406aebb8
Martin Rychlovsky <masak@gravastar.cz>
2013-03-07 16:09:33 +0100

    Vypnuty example Deface

--- app/deface/site_decorato_example.rb
+++ app/deface/site_decorato_example.rb
@@ -1,4 +1,4 @@
-Deface::Override.new(:virtual_path => "spree/layouts/spree_application", 
-                     :name => "head_override", 
-                     :insert_before => "div#content", 
-                     :text => "<h1>Tak tohle jsem vlepil do stranky!</h1>")
+#Deface::Override.new(:virtual_path => "spree/layouts/spree_application", 
+#                     :name => "head_override", 
+#                     :insert_before => "div#content", 
+#                     :text => "<h1>Tak tohle jsem vlepil do stranky!</h1>")
